c2a00cbdd7962731716923e158fbb4a76882253c
soma <soma@koii.network>
2023-09-25 17:11:09 -0300

    update generateDistributionList function

--- coreLogic.js
+++ coreLogic.js
@@ -76,19 +76,22 @@ class CoreLogic {
       /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******/
 
       let distributionList = {};
+      let distributionCandidates = [];
       let taskAccountDataJSON = await namespaceWrapper.getTaskState();
       if (taskAccountDataJSON == null) taskAccountDataJSON = _dummyTaskState;
       const submissions = taskAccountDataJSON.submissions[round];
       const submissions_audit_trigger =
         taskAccountDataJSON.submissions_audit_trigger[round];
       if (submissions == null) {
-        console.log('No submisssions found in N-2 round');
+        console.log(`No submisssions found in round ${round}`);
         return distributionList;
       } else {
         const keys = Object.keys(submissions);
         const values = Object.values(submissions);
         const size = values.length;
         console.log('Submissions from last round: ', keys, values, size);
+
+        // Logic for slashing the stake of the candidate who has been audited and found to be false
         for (let i = 0; i < size; i++) {
           const candidatePublicKey = keys[i];
           if (
@@ -96,32 +99,56 @@ class CoreLogic {
             submissions_audit_trigger[candidatePublicKey]
           ) {
             console.log(
-              submissions_audit_trigger[candidatePublicKey].votes,
               'distributions_audit_trigger votes ',
+              submissions_audit_trigger[candidatePublicKey].votes,
             );
             const votes = submissions_audit_trigger[candidatePublicKey].votes;
-            let numOfVotes = 0;
-            for (let index = 0; index < votes.length; index++) {
-              if (votes[index].is_valid) numOfVotes++;
-              else numOfVotes--;
+            if (votes.length === 0) {
+              // slash 70% of the stake as still the audit is triggered but no votes are casted
+              // Note that the votes are on the basis of the submission value
+              // to do so we need to fetch the stakes of the candidate from the task state
+              const stake_list = taskAccountDataJSON.stake_list;
+              const candidateStake = stake_list[candidatePublicKey];
+              const slashedStake = candidateStake * 0.7;
+              distributionList[candidatePublicKey] = -slashedStake;
+              console.log('Candidate Stake', candidateStake);
+            } else {
+              let numOfVotes = 0;
+              for (let index = 0; index < votes.length; index++) {
+                if (votes[index].is_valid) numOfVotes++;
+                else numOfVotes--;
+              }
+
+              if (numOfVotes < 0) {
+                // slash 70% of the stake as the number of false votes are more than the number of true votes
+                // Note that the votes are on the basis of the submission value
+                // to do so we need to fetch the stakes of the candidate from the task state
+                const stake_list = taskAccountDataJSON.stake_list;
+                const candidateStake = stake_list[candidatePublicKey];
+                const slashedStake = candidateStake * 0.7;
+                distributionList[candidatePublicKey] = -slashedStake;
+                console.log('Candidate Stake', candidateStake);
+              }
+
+              if (numOfVotes > 0) {
+                distributionCandidates.push(candidatePublicKey);
+              }
             }
-            if (numOfVotes < 0) continue;
+          } else {
+            distributionCandidates.push(candidatePublicKey);
           }
-
-          // now we need to parse the value submitted and decide how much to pay
-          let cid = values[i].submission_value;
-          console.log(`about to fetch ${cid} from IPFS`)
-          // let ipfs_object = this.twitterTask.getJSONofCID(cid);
-          // if (ipfs_object == null || !ipfs_object) {
-          //   distributionList[candidatePublicKey] = 0;
-          // } else {
-          //   if (ipfs_object.length == null || ipfs_object.length < 1) ipfs_object.length = 1;
-          //   let score = ipfs_object.length * 0.1; // multiply total records submitted by value per record (0.1 KOII)
-          //   distributionList[candidatePublicKey] = score;
-          // }
-
-          distributionList[candidatePublicKey] = 1 * LAMPORTS_PER_SOL;
         }
+      }
+
+      // now distribute the rewards based on the valid submissions
+      // Here it is assumed that all the nodes doing valid submission gets the same reward
+
+      const reward =
+        taskAccountDataJSON.bounty_amount_per_round /
+        distributionCandidates.length;
+      console.log('REWARD RECEIVED BY EACH NODE', reward);
+      for (let i = 0; i < distributionCandidates.length; i++) {
+        distributionList[distributionCandidates[i]] = reward;
       }
       console.log('Distribution List', distributionList);
       return distributionList;
