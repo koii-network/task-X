39af355a8835905196f3865dfd39b1bb1bbedf07
Crystal Parker <crystal@koii.network>
2024-02-21 14:23:45 -0400

    removed line in index.js for routes.js which was deleted because it didn't do anything and just held dummy data

--- index.js
+++ index.js
@@ -43,16 +43,12 @@ if (taskNodeAdministered) {
 
 if (app) {
   //  Write your Express Endpoints here.
-  //  For Example
-  //  app.post('/accept-cid', async (req, res) => {})
 
   // Sample API that return your task state
-
   app.get('/taskState', async (req, res) => {
     const state = await namespaceWrapper.getTaskState();
     console.log('TASK STATE', state);
 
     res.status(200).json({ taskState: state });
   });
-  app.use('/api/', require('./routes') );
 }
