60b9d833971dccd504325b183292c7f90e9212a8
Soma Li <soma@koii.network>
2024-05-16 16:10:22 -0300

    update namespaceWrapper

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (app) {
   // Sample API that return your task state
 
   app.get('/taskState', async (req, res) => {
-    const state = await namespaceWrapper.getTaskState();
+    const state = await namespaceWrapper.getTaskState({});
     console.log('TASK STATE', state);
 
     res.status(200).json({ taskState: state });
