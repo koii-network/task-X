a4b3a8177120c789845feb9d4a68f59aae8a3bec
yiqun Liang <herman@koii.network>
2024-05-27 23:50:10 +0100

    update getFile

--- twitter-task.js
+++ twitter-task.js
@@ -223,9 +223,11 @@ const getJSONFromCID = async (
   console.log(urllist);
   const client = new KoiiStorageClient(undefined, undefined, true);
   try {
-    const data = await client.getFile(cid, fileName);
+    const blob = await client.getFile(cid, fileName);
+    const text = await blob.text(); // Convert Blob to text
+    const data = JSON.parse(text); // Parse text to JSON
     return data;
-  } catch (error) {
+  }  catch (error) {
     console.log(`Error fetching file from Koii IPFS: ${error.message}`);
   }
   for (const url of urllist) {
