2cd33466b1de0889244036549be778f430f76abf
yiqun Liang <herman@koii.network>
2024-06-06 15:30:57 +0100

    updated to 0.7.1

--- coreLogic.js
+++ coreLogic.js
@@ -71,7 +71,7 @@ class CoreLogic {
       try {
         taskAccountDataJSON = await namespaceWrapper.getTaskSubmissionInfo(
           round,
-          forcefetch=true
+          true
         );
       } catch (error) {
         console.error('ERROR IN FETCHING TASK SUBMISSION DATA', error);
