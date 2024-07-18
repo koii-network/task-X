06d4eba2c7e2500b5481b9c740045d9d327fbe31
Herman Liang <herman@koii.network>
2024-07-18 13:20:27 -0300

    change wording

--- adapters/twitter/twitter.js
+++ adapters/twitter/twitter.js
@@ -789,7 +789,7 @@ class Twitter extends Adapter {
           return false;
         }
         if (result.time_read - inputitem.time_read > 3600000 * 15) {
-          console.log("time read not match", result.time_read, inputitem.time_read);
+          console.log("time read difference too big", result.time_read, inputitem.time_read);
           return false;
         }
         const dataToCompare = result.tweets_content + result.time_post;
