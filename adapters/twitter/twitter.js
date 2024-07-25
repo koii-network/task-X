81d96515ce2496ddaef30f2fabb86ac3fa40aee4
Herman Liang <herman@koii.network>
2024-07-25 15:06:48 -0300

    Edit hash wording

--- adapters/twitter/twitter.js
+++ adapters/twitter/twitter.js
@@ -85,7 +85,7 @@ class Twitter extends Adapter {
       this.browser = await stats.puppeteer.launch({
         executablePath: stats.executablePath,
         userDataDir: userDataDir,
-        headless: false,
+        // headless: false,
         userAgent:
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         args: [
@@ -750,7 +750,7 @@ class Twitter extends Adapter {
       let auditBrowser = await stats.puppeteer.launch({
         executablePath: stats.executablePath,
         userDataDir: userAuditDir,
-        headless: false,
+        // headless: false,
         userAgent:
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         args: [
@@ -796,8 +796,9 @@ class Twitter extends Adapter {
     
         const hashCompare = bcrypt.compareSync(dataToCompare, inputitem.hash);
         if(hashCompare==false){
-          console.log("Hash not match", dataToCompare, inputitem.hash);
+          console.log("Hash Verification Failed", dataToCompare, inputitem.hash);
           auditBrowser.close();
+          return false;
         }
         auditBrowser.close();
         return true;
